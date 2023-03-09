96b5ed797558204d39e5d5b45aa0bd23d9960867
Gilbert Ye <gilbert.ye@mail.utoronto.ca>
2023-03-08 19:28:43 -0500

    added more models

--- P2/restify/accounts/models.py
+++ P2/restify/accounts/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from properties.models import Comment
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 class User(AbstractUser):
     phoneNumber = models.CharField(max_length = 10) # or an int? include dashes?
     avatars = models.ImageField(upload_to='avatars/', null=True, blank = True) # add avatars/pictures folder
+    comments = GenericRelation(Comment)
 
     def __str__(self):
         return f"{self.username}"
