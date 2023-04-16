8343cb9feb6253d2ca9982b9943d812572dffdaf
Gilbert Ye <gilbert.ye@mail.utoronto.ca>
2023-04-15 22:47:59 -0400

    notif and reserve

--- P3/frontend/src/components/reservationcard.jsx
+++ P3/frontend/src/components/reservationcard.jsx
@@ -1,10 +1,57 @@
-function Reserve() {
+import { useEffect, useState } from "react";
+
+function Reserve(property_id, price, rating) {
+    const [property, setProperty] = useState([]);
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [totalDays, setTotalDays] = useState(null);
+    const [totalPrice, setTotalPrice] = useState(null);
+    const [tax, setTaxes] = useState(null);
+    const [sumPrice, setSumPrice] = useState(null);
+
+
+    useEffect(() => {
+        if (startDate && endDate && price) {
+          const start = new Date(startDate);
+          const end = new Date(endDate);
+    
+          const diffInMs = end.getTime() - start.getTime();
+          const diffInDays = Math.ceil(diffInMs / (1000 * 60 * 60 * 24));
+          setTotalDays(diffInDays);
+          setTotalPrice(diffInDays * price);
+          setTaxes(diffInDays * 0.05);
+          setSumPrice(tax + totalPrice);
+        }
+    }, [startDate, endDate, price]);
+
+    
+    useEffect(() => {
+        fetch(process.env.REACT_APP_BACKEND_API + 'properties/property/view/#{property_id}')
+        .then(response => {
+            console.log(response);
+            if (response.status === 200) {
+              return response.json();
+            } else if (response.status === 404) {
+              throw new Error('Requested resource not found');
+            } else {
+              throw new Error('Server error');
+            }
+          })
+        .then(json => {
+            console.log("yay");
+            setProperty(json.data);
+        }).catch(error => {
+            console.log("no");
+            console.log(error);
+        })
+    }, [property])
+
     return (
         <span class = "border">
             <div class="col-lg-4">
             <h5 >Reserve</h5>
             <h6 >
-                $200 CAD/day | ★ 3.0
+                ${price}CAD/day | ★ {rating}
             </h6>
 
 
@@ -30,10 +77,10 @@ function Reserve() {
                 <div class="row">
                     <div class="col-3 ">
                         <div>Check-in Date</div>
-                    </div>
+                    </div> 
                     <div class="col-6 ">
-                        <input type="date" class="form-control" id="reserveDateInInput"
-                            placeholder="Check In Date" min="2023-02-01" max="2026-12-31"></input>
+                        <input type="date" class="form-control" id="startDate" min="2023-02-01" max="2026-12-31" 
+                        value={startDate} onChange={(event) => setStartDate(event.target.value)} />
                     </div>
                 </div>
 
@@ -42,17 +89,17 @@ function Reserve() {
                         <div>Check-out Date</div>
                     </div>
                     <div class="col-6 ">
-                        <input type="date" class="form-control" id="reserveDateOutInput"
-                            placeholder="Check In Date" min="2023-02-01" max="2026-12-31"></input>
+                        <input type="date" class="form-control" id="endDate" min="2023-02-01" max="2026-12-31" 
+                        value={endDate} onChange={(event) => setEndDate(event.target.value)} />
                     </div>
                 </div>
 
                 <div class="row pt-3">
                     <div class="col-3 ">
-                        <div>$200 CAD * 3 Days</div>
+                        <div>${price} CAD * {totalDays} Days</div>
                     </div>
                     <div class="col-6 ">
-                        <div>$600 CAD</div>
+                        <div>${totalPrice} CAD</div>
                     </div>
                 </div>
 
@@ -62,7 +109,7 @@ function Reserve() {
                         <div>Taxes</div>
                     </div>
                     <div class="col-6 ">
-                        <div>$100 CAD</div>
+                        <div>${tax} CAD</div>
                     </div>
                 </div>
 
@@ -72,10 +119,29 @@ function Reserve() {
                         <div>Total</div>
                     </div>
                     <div class="col-6 bold">
-                        <div>$700 CAD</div>
+                        <div>${sumPrice} CAD</div>
                     </div>
                 </div>
             </div>
+            <button type="button" class="btn-btn button-darken border-0" aria-label="Close" style={{background: '#85bded'}} onClick={() => 
+                    fetch(process.env.REACT_APP_BACKEND_API +  'properties/reservation-create/', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            property: {property_id},
+                            res_start_time: {startDate},
+                            res_end_time: {endDate}, 
+                            price: {price}
+                        })
+                        })
+                        .then(response => {
+                        // handle response
+                        })
+                        .catch(error => {
+                        // handle error
+                        })}></button>
             <button class="btn button-darken border-0" style={{background: '#85bded'}}>Reserve</button>
         </div>
         </span>
